e8b549bcf50eda57893d63c15f8dfa497e78cadd
natepecota <nate.pecota@gmail.com>
2015-05-29 14:07:15 -0700

    Change server listening message in server.js

--- server.js
+++ server.js
@@ -45,6 +45,6 @@ var server = app.listen(process.env.PORT || 5000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('I am listening at http://%s:%s', host, port);
 
 });
